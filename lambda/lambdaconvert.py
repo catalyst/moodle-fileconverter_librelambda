3e96bdf7a9c059bb842444203aed3762eec206c6
Matt Porritt <mattp@catalyst-au.net>
2018-12-15 12:29:03 +1100

    WIP: lambda funciton

--- lambda/lambdaconvert.py
+++ lambda/lambdaconvert.py
@@ -1,18 +1,39 @@
 import boto3
+import botocore
+import json
+import zipfile
+import tempfile
 import os
-import sys
-import uuid
+import mimetypes
+import logging
 
 s3_client = boto3.client('s3')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-def handler(event, context):
+
+def lambda_handler(event, context):
+    """
+    lambda_handler is the entry point that is invoked when the lambda function is called,
+    more information can be found in the docs:
+    https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
+
+    Get the input document from the input S3 bucket. Convert the input file into the desired format.
+    Upload the converted document to the output S3 bucket.
+    """
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
+        targetformat = record['s3']['object']['Metadata']['targetformat']
+        conversionid = record['s3']['object']['Metadata']['id']
+        sourcefileid = record['s3']['object']['Metadata']['sourcefileid']
+
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/tmp/resized-{}'.format(key)
+        # upload_path = '/tmp/resized-{}'.format(key)
+        upload_path = download_path
 
         s3_client.download_file(bucket, key, download_path)
 
-        s3_client.upload_file(upload_path, '{}resized'.format(bucket), key)
+        # TODO: pass the output bucket name in at runtime.
+        s3_client.upload_file(upload_path, 'librelamdamoodle-output', key)
