25856c22c23eca8138e03cf2fe8ce8b775febabf
Matt Porritt <mattp@catalyst-au.net>
2019-04-11 14:58:12 +0000

    add initial script to get LibreOffice source

---
+++ libre/cli/getsource.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This script gets the source code of LibreOffice.
+
+curl -L https://github.com/LibreOffice/core/archive/libreoffice-6.2.3.2.tar.gz | tar -xz
+mv core-libreoffice-6.2.3.2 libreoffice
+
+# See https://ask.libreoffice.org/en/question/72766/sourcesver-missing-while-compiling-from-source/
+echo "lo_sources_ver=6.2.3.2" >> libreoffice/sources.ver
