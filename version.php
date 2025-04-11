8c7b6bb9b79f7ae75fd7454aae49362b769d7616
guillaumebarat <guillaumebarat@catalyst-au.net>
2025-04-11 15:22:32 +1000

    version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'fileconverter_librelambda';
-$plugin->release = 2023010900;
-$plugin->version = 2023010900;
+$plugin->release = 2025041100;
+$plugin->version = 2025041100;
 $plugin->requires = 2017111309;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
