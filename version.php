3431225be5fff31445d3fd34f27d8c99e8ceb877
guillaumebarat <guillaumebarat@catalyst-au.net>
2025-07-17 13:01:03 +1000

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'fileconverter_librelambda';
-$plugin->release = 2024111300;
-$plugin->version = 2024111300;
+$plugin->release = 2024111301;
+$plugin->version = 2024111301;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [405, 405]; // A range of branch numbers of supported moodle versions.
